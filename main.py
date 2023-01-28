2dba1aaf8f16f5cb41fe571e61c22f13126b1364
Rohan Shah <rshah04@gmail.com>
2023-01-28 17:27:37 -0500

    did stuff

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 import time, sys, random  
+from rich import print
+from rich.panel import Panel
+from rich.text import Text
+from rich.prompt import Prompt
 
 from random import randint
 
-def cthulu():
-  print("")
 
-def printer(str):
+def printer(str, style= "bold green"):
     for letter in str:  
-        sys.stdout.write(letter)
-        sys.stdout.flush()
+        modded_letter = Text(letter, style)
+        print(modded_letter, end="", flush=True)
         time.sleep(0.001)
         # make time slower later 
     print("\n")
@@ -21,21 +23,17 @@ def preamble():
   printer("There is no one inside. \nNo clothing, no bodies, no scatter of papers, no trace of any life.")
   printer("You are investigative reporter, Sharon Sterling. \nDucking under the police tape, you make your way to the scene. Just as reported, there is no evidence inside.")
   printer("All that remains is the ship's computer interface. Incredibly, it still boots up when you click the power button. \nYou take your place in front of the screen. What happened to this ship? To the crew? It all rests on you to find out.")
-  username = input("INPUT USERNAME: ")
-  password = input("INPUT PASSWORD: ")
+  username = Text("ENTER USERNAME", style= "bold red")
+  password = Text("ENTER PASSWORD", style= "bold red")
+  askUsername = Prompt.ask(username)
+  askPassword = Prompt.ask(password)
 
-  printer("\nINCORRECT USERNAME OR PASSWORD")
+  printer("\nINCORRECT USERNAME OR PASSWORD", style= "bold red")
   printer("CONCEALING AND ENCRYPTING CREW LOGS")
   printer("FOR THE PROTECTION OF STATE SECRETS, CREW LOGS ARE ONLY AVAILABLE THROUGH KEYWORD SEARCH. WORKERS OF THE WORLD, UNITE.")
 
 def keywordSearch():
   chosenWord = input("ENTER KEYWORD: ")
-
-
-
-
-
-
 
 
 
@@ -46,16 +44,28 @@ keywordSearch()
 keywords = {"test",}
 
 story = "yadda yadda yadda important words here. This sentence contains test, the whole thing should be present. These should show, these should not."
+storyList = story.split()
 
-def findWord(keyword):
-  wordIndex = story.find(keyword)
+def getContextSentence(keyword):
+  wordIndex = storyList.index(keyword)
 
   if wordIndex == -1:
     printer("ERROR: KEYWORD NOT FOUND")
   else:
     # find previous period to see start of last sentence
-    lastPeriod = story.rfind(".", 0, wordIndex)
-    printer(story[lastPeriod - 4: lastPeriod])
+    printer(f"{keyword} found at index {wordIndex}")
 
 
-findWord("test")
+getContextSentence("test")
+
+
+def chooseATextToReturn(ListOfPairs):
+  fullCount = 0
+  indexList =[]
+  for i in range(len(ListOfPairs)):
+    fullCount += ListOfPairs[i][1]
+    for j in range(ListOfPairs[i][1]):
+      indexList.append(i)
+  chosenNumber = randint(0, fullCount - 1)
+  return ListOfPairs[chosenNumber][1]
+  
