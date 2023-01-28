ce78adf20d3e9050060bfec21555c5ec2e655dbb
Herschenglime <herschenglime@gmail.com>
2023-01-28 16:40:22 -0500

    in poggers word search

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import time, sys, random  
 
 from random import randint
@@ -19,7 +20,7 @@ def preamble():
   printer("The Santa Monica pier bustles with its usual beach bodies. As the tide of the Pacific recedes, a large metal object is revealed. \nWithin minutes, local news has arrived to capture the scene. A K-278 Komsomolets Soviet submarine has washed ashore in California.")
   printer("There is no one inside. \nNo clothing, no bodies, no scatter of papers, no trace of any life.")
   printer("You are investigative reporter, Sharon Sterling. \nDucking under the police tape, you make your way to the scene. Just as reported, there is no evidence inside.")
-  printer("All that remains is the ship’s computer interface. Incredibly, it still boots up when you click the power button. \nYou take your place in front of the screen. What happened to this ship? To the crew? It all rests on you to find out.")
+  printer("All that remains is the ship's computer interface. Incredibly, it still boots up when you click the power button. \nYou take your place in front of the screen. What happened to this ship? To the crew? It all rests on you to find out.")
   username = input("INPUT USERNAME: ")
   password = input("INPUT PASSWORD: ")
 
@@ -31,7 +32,7 @@ def keywordSearch():
   chosenWord = input("ENTER KEYWORD: ")
 
 
-#
+
 
 
 
@@ -41,8 +42,20 @@ def keywordSearch():
 preamble()
 keywordSearch()
 
-#!/usr/bin/env python3
+
+keywords = {"test",}
+
+story = "yadda yadda yadda important words here. This sentence contains test, the whole thing should be present. These should show, these should not."
+
+def findWord(keyword):
+  wordIndex = story.find(keyword)
+
+  if wordIndex == -1:
+    printer("ERROR: KEYWORD NOT FOUND")
+  else:
+    # find previous period to see start of last sentence
+    lastPeriod = story.rfind(".", 0, wordIndex)
+    printer(story[lastPeriod - 4: lastPeriod])
 
 
-
-
+findWord("test")
