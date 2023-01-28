8742fc29e54e8ef0b3f0784b4d1badab6f646588
Rohan Shah <rshah04@gmail.com>
2023-01-28 13:32:08 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import time, sys, random
+
+
+def printer(s):
+  for i in range(len(s)):
+    while 1:
+      time.sleep(0.0001)
+      print(s[i])
+
+
+
+    
+  
+
+def preamble():
+  print("January 29th, 2023") 
+  print()
+  print("The Santa Monica pier bustles with its usual beach bodies. As the tide of the Pacific recedes, a large metal object is revealed.")
+  print("Within minutes, local news has arrived to capture the scene. A K-278 Komsomolets Soviet submarine has washed ashore in California.") 
+  print()
+  print("There is no one inside.")
+  print("No clothing, no bodies, no scatter of papers, no trace of any life.")
+  print()
+  print("You are investigative reporter, Sharon Sterling.")
+  print("Ducking under the police tape, you make your way to the scene. Just as reported, there is no evidence inside.")
+  print()
+  print("All that remains is the ship’s computer interface. Incredibly, it still boots up when you click the power button.")
+  print("You take your place in front of the screen. What happened to this ship? To the crew? It all rests on you to find out.") 
+
+preamble()
